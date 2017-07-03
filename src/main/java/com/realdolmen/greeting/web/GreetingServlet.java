23a769078e72e95a48c00be6b0d0e961384cc2c0
Tjen Dekker <vontjen@gmail.com>
2017-07-03 14:25:56 +0200

    commit from class

--- src/main/java/com/realdolmen/greeting/web/GreetingServlet.java
+++ src/main/java/com/realdolmen/greeting/web/GreetingServlet.java
@@ -1,23 +1,22 @@
 package com.realdolmen.greeting.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
-/**
- * Created by vdabcursist on 03/07/2017.
- */
+@WebServlet("/hello")
 public class GreetingServlet extends HttpServlet{
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("Hello World");
+
 
     }
 
-    @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
-    }
 }
